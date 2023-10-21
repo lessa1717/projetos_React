716f2fe1846f8f36881e9d2de28abbbd3bcbd2a0
lessa1717 <lessa07oliveira@gmail.com>
2023-10-21 12:20:30 -0300

    Alteracao com responsividade

--- src/Home.js
+++ src/Home.js
@@ -10,13 +10,15 @@ function Home() {
             <ul>
                 <li>
                     <img src="https://img.elo7.com.br/product/original/264FCC6/big-poster-filme-batman-o-cavaleiro-das-trevas-lo02-90x60-cm-batman.jpg" alt="Filme Batman"/>
-                    <span></span>
+                    <span>Batman Dark Knight</span>
                 </li>
                 <li>
                     <img src="https://i.pinimg.com/originals/29/db/0c/29db0c0a687e36fd8a03f01d367fb14c.jpg" alt="Filme Viva a vida é uma festa"></img>
+                    <span>Viva a vida é uma festa</span>
                 </li>
                 <li>
                     <img src="https://johto.legiaodosherois.com.br/wp-content/uploads/2023/01/legiao_jIOfk42XMuUm-scaled.jpg" alt="Filme Pânico 6"></img>
+                    <span>Pânico 6</span>
                 </li>
             </ul>
 
--- src/style.js
+++ src/style.js
@@ -10,7 +10,7 @@ h2{
 ul{
     list-style: none;
     display: grid;
-    grid-template-columns: repear(3, 1fr);
+    grid-template-columns: repeat(auto-fit,minmax(200px,1fr));
     column-gap: 3rem;
     row-gap: 4rem;
 
